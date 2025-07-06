d26a1872562f48a22d8120fea2dc044e06b84834
Tim Kelley <ctk@ncsu.edu>
2025-07-06 10:09:57 -0400

    First code drop

---
+++ Codes/sum_test.jl
@@ -0,0 +1,15 @@
+function sum_test(n=1000)
+# M2 Mac using the Apple Accelerate Blas:w
+# random Float16 vector
+vh=rand(Float16, n);
+vh ./  n
+# double precision copy
+vd=Float64.(vh)
+# sum in half, sum in double
+sumh=sum(vh)
+sumd=sum(vd)
+println(sumh,"  ", sumd)
+#compare results
+del=abs(sumd-sumh)/sumd
+return del
+end
